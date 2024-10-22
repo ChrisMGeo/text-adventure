5c70b23fece9ad6f1d3da7689a467f13dbec0b17
Chris George <chrismathew98@gmail.com>
2024-10-22 00:06:02 -0400

    Add UI for dialogues, and add automatic color palette for it.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import { defaultCharacters, defaultMoods, defaultScenes } from '@/consts/defaults';
-import { useTextAdventure } from '@/hooks/use-text-adventure';
-import { getSpeakers } from '@/util/converters';
-import { useState } from 'react';
+import { TextAdventureLog, useTextAdventure } from '@/hooks/use-text-adventure';
+import { getCharacterImage, getSceneImage } from '@/util/image-getters';
+import { useEffect, useState } from 'react';
+import Image from 'next/image'
+import { usePalette } from 'react-palette';
 
 export default function Chat() {
   const { log, submitMessage, loading } = useTextAdventure({
@@ -18,31 +20,77 @@ export default function Chat() {
       submitMessage(input);
       setInput('');
     }
+    setLogIndex(logIndex + 1);
   }
-  return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {/* {messages.map(m => ( */}
-      {/*   <div key={m.id} className="whitespace-pre-wrap"> */}
-      {/*     {m.role === 'user' ? 'User: ' : 'AI: '} */}
-      {/*     {m.content} */}
-      {/*   </div> */}
-      {/* ))} */}
-      {log.map((l, i) => (
-        <div key={i} className="whitespace-pre-wrap">
-          {l.type === 'input' ? '> ' : getSpeakers(l.value, defaultCharacters) + ": "}
-          {l.type === 'input' ? l.value : l.value.content}
+  const dialogueLogs = log.filter(l => l.type === 'dialogue');
+  const [logIndex, setLogIndex] = useState(0);
+  const [bg, setBg] = useState('');
+  const [speakers, setSpeakers] = useState<{ character: string; mood: string }[]>([]);
+  const [content, setContent] = useState('');
+  const isAtEnd = logIndex >= dialogueLogs.length - 1;
+  useEffect(() => {
+    const currentLog: TextAdventureLog | undefined = dialogueLogs[logIndex];
+    if (currentLog) {
+      const { speaker, content, scene, mood } = currentLog.value;
+      switch (speaker.type) {
+        case 'single':
+          setSpeakers([{ character: speaker.character, mood: speaker.mood }]);
+          break;
+        case 'multiple':
+          setSpeakers(speaker.value);
+          break;
+      }
+      setContent(content);
+      const bgRes = getSceneImage(defaultScenes, scene, mood);
+      if (bgRes) setBg(bgRes);
+    }
+  }, [logIndex, log]);
+  const characterImages = speakers.map(({ character, mood }) => getCharacterImage(defaultCharacters, character, mood)).filter(c => c !== undefined);
+  console.log(characterImages);
+  const leftImages = characterImages.filter((_, i) => i % 2 === 0);
+  const rightImages = characterImages.filter((_, i) => i % 2 === 1);
+  const disableInput = loading || !isAtEnd;
+  const { data: palette } = usePalette(bg);
+  return <div className="relative w-full h-screen overflow-hidden" style={{
+    backgroundImage: `url(${bg})`,
+    backgroundSize: 'cover',
+  }}>
+    <div className="flex flex-col h-full">
+      <div className="flex flex-row flex-grow">
+        <div className="flex items-end">
+          <Image src={leftImages[0]?.url} alt="Character Image" width={500} height={250} />
         </div>
-      ))}
-
-      <form onSubmit={handleSubmit}>
-        <input
-          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
-          value={input}
-          placeholder="Say something..."
-          onChange={(e) => setInput(e.target.value)}
-          disabled={loading}
-        />
-      </form>
+        <div className="flex-grow bg-cover bg-center hidden md:flex" />
+        <div className="hidden md:flex md:items-end" >
+          <Image src={rightImages[0]?.url} alt="Character Image" width={500} height={250} />
+        </div>
+      </div>
+      <div className="p-4 rounded-xl border-solid border-4" style={{
+        backgroundColor: palette.lightVibrant,
+        color: palette.darkMuted,
+        borderColor: palette.darkVibrant
+      }}>
+        <h3 className="text-xl font-bold mb-2">
+          {speakers.map(({ character, mood }) => {
+            const characterData = defaultCharacters.find(c => c.id === character);
+            const moodData = defaultMoods.find(m => m.id === mood);
+            if (!characterData || !moodData) return null;
+            return characterData.name;
+          }).join(', ')}
+        </h3>
+        <p className="text-lg mb-4">{content}</p>
+        {!isAtEnd && <button onClick={() => {
+          setLogIndex(logIndex => logIndex + 1)
+        }}>Next</button>}
+        {!disableInput && <form onSubmit={handleSubmit} className="flex space-x-2">
+          <input className="flex-grow rounded-xl p-2 border-solid border-2" type="text" value={input} placeholder="Say something..." onChange={(e) => setInput(e.target.value)} disabled={disableInput} style={{
+            backgroundColor: palette.lightVibrant,
+            color: palette.darkMuted,
+            borderColor: palette.darkVibrant
+          }} />
+          <button type="submit" disabled={disableInput}>Submit</button>
+        </form>}
+      </div>
     </div>
-  );
+  </div>
 }
--- src/util/image-getters.ts
+++ src/util/image-getters.ts
@@ -1,19 +1,19 @@
 import { SceneData } from "@/types/scene-data";
 import { CharacterData } from "@/types/character-data";
 
-export const getSceneImage = (allSceneData: SceneData[], sceneId: string, moodId?: string): string => {
+export const getSceneImage = (allSceneData: SceneData[], sceneId: string, moodId?: string): string | undefined => {
   const scene = allSceneData.find((scene) => scene.id === sceneId);
-  if (!scene) return "";
-  if (moodId && scene.images?.moods[moodId]) {
+  if (!scene) return undefined;
+  if (moodId && scene.images?.moods?.[moodId]) {
     return scene.images.moods[moodId].url;
   }
-  return scene.images?.default.url || "";
+  return scene.images?.default.url;
 }
 
 export const getCharacterImage = (allCharacterData: CharacterData[], characterId: string, moodId?: string): { url: string, direction: "left" | "right" } | undefined => {
   const character = allCharacterData.find((character) => character.id === characterId);
   if (!character) return undefined;
-  if (moodId && character.images?.moods[moodId]) {
+  if (moodId && character.images?.moods?.[moodId]) {
     return { url: character.images.moods[moodId].url, direction: character.images.moods[moodId].facingDirection };
   }
   if (character.images?.default) {
