d14c8212a5991733d7293069fdce3d2dda853a7c
Chris George <chrismathew98@gmail.com>
2024-10-22 00:04:05 -0400

    Get rid of unused models

--- src/consts/langchain.ts
+++ src/consts/langchain.ts
@@ -3,22 +3,12 @@ import { MoodData } from "@/types/mood-data";
 import { SceneData } from "@/types/scene-data";
 import { zDialogueSchemaArrayStrict } from "@/zod-schemas/dialogue-schema";
 import { ChatOpenAI } from "@langchain/openai";
-import { ChatGroq } from "@langchain/groq";
-import { ChatMistralAI } from "@langchain/mistralai";
 import { z } from "zod";
 
 const maxRetries = 10;
 
 const openaiModel = new ChatOpenAI({
   model: process.env.OPENAI_MODEL,
-  maxRetries
-});
-const groqModel = new ChatGroq({
-  model: process.env.GROQ_MODEL,
-  maxRetries
-});
-const mistralaiModel = new ChatMistralAI({
-  model: process.env.MISTRALAI_MODEL,
   maxRetries
 });
 
